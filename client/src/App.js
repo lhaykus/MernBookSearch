8c82f258c24cbbc311a870cb5ebbad3eff2a0d59
Loryn <lorynhaykus@yahoo.com>
2021-09-26 18:12:33 -0600

    added apollo to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+//Import ApolloServer
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//Create main graphQl API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//Request middleware that will attach token to every request
+const authLink = setContext((_, { headers }) => {
+  //get authentication token from local storage
+  const token = localStorage.getItem('id_token');
+  //return headers to context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer${token}` : '',
+    },
+  };
+});
+
+//Set up client to execute the middleware before making GraphQL API request
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    //Wrap with ApolloProvider
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +45,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
