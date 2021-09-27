fd7769441e871fd40d5c34a79065c4d937f8c14a
Loryn <lorynhaykus@yahoo.com>
2021-09-26 18:28:43 -0600

    added mutations to signup and login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,17 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
+//Import mutations
+import { LOGIN } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+
 const LoginForm = () => {
+  //login mutation
+  const [ login ] = useMutation(LOGIN);
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,15 +33,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
