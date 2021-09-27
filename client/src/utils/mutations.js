6082dafe63b3303ed572a392594ce80269d3298c
Loryn <lorynhaykus@yahoo.com>
2021-09-26 18:02:22 -0600

    created queries and mutations on client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email:$email, password:$password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const LOGIN = gql`
+    mutation login($email:String!, $password: String!) {
+        login(email:$email, password:$password) {
+            token
+            user {
+                _id
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($id: ID!, $input: bookInput!) {
+        saveBook(id: $id, input: $input) {
+            savedBooks {
+                title
+                description
+            }
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($id: ID!, $bookId: String!) {
+        deleteBook(id: $id, bookId: $bookId) {
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
